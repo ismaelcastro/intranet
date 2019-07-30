17805b6965b731fd7931b4c53ac37585a13815c5
ismael castro <ismael_castro@outlook.com.br>
2019-07-30 05:21:25 -0300

    finalizando calendar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @stop
 @section('content')
     <div class="row">	
-		<div class="col-md-9">	
+		<div class="col-md-6">	
 			<div class="box box-primary">	
 				<div class="box box-body no-padding">	
 						<div id="calendar">	
@@ -23,12 +23,33 @@
 	<script src="{{ URL::asset('plugins/moment/moment.js') }}"></script>
 	<script src="{{ URL::asset('plugins/fullcalendar/dist/fullcalendar.min.js')}}"></script>
 	<script>
+		var date = new Date()
+    	var d    = date.getDate(),
+        	m    = date.getMonth(),
+        	y    = date.getFullYear()
 		$('#calendar').fullCalendar({
 			header    : {
         		left  : 'prev,next today',
         		center: 'title',
         		right : 'month,agendaWeek,agendaDay'
       		},
+      		buttonText:{
+      			today:"Hoje",
+      			month:"Mês",
+      			week:"Semana",
+      			day:"Dia",
+      		},
+      		events: [
+      			{
+      				title: "Férias Ismael Castro",
+      				start: "2019-07-30",
+      				end:"2019-08-29",
+      				backgroundColor: "#f39c12",
+      				boderColor: "#f39c12",
+      			},
+
+      		],
+      		displayEventTime:false,
 		});
 	</script>
 @endpush
