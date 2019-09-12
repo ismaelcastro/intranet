f40cd1cdf3d763d5ea9dd818f20c21d8f34481b7
ismaelcastro <ismael_castro@outlook.com.br>
2019-09-12 15:49:55 -0300

    grafico de faturamento

---
+++ public/js/comercial/comercial.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	var line = new Morris.Line({
+		element 		 : 'faturamento',
+		resize			 : true,
+		data 			 :[
+			{y: '2019-04-30', proel: 2000, sh: 3000, rep: 4000},
+        	{y: '2019-05-30', proel: 3000, sh: 2000, rep: 7000},
+        	{y: '2019-06-30', proel: 4000, sh: 4000, rep: 9000},
+		],
+		xkey			 :'y',
+		ykeys 			 :['proel', 'sh', 'rep'],
+		labels			 : ['Proel', 'SH', 'Repre'],
+		lineColors 		 : ['#0288D1', '#00695C', '#FF8F00'],
+		lineWidth  		 : 2,
+		hideHover        : 'auto',
+		gridTextColor    : '#fff',
+		gridLineColor    : '#efefef',
+		xLabels 		 : 'month',
+		xLabelFormat	 : function(x){
+			var IndexToMonth = [ "Janeiro", "Fevereiro", "Mar", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" ];
+			var month = IndexToMonth[ x.getMonth() ];
+			return month;
+
+		},
+    	
+    	
+    	
+    	
+    	
+	});
+
+	$('.knob').knob();
+
+	/*var line = new Morris.Line({
+      element: 'faturamento',
+      resize: true,
+      data: [
+        {y: '2011 Q1', item1: 2666},
+        {y: '2011 Q2', item1: 2778},
+        {y: '2011 Q3', item1: 4912},
+        {y: '2011 Q4', item1: 3767},
+        {y: '2012 Q1', item1: 6810},
+        {y: '2012 Q2', item1: 5670},
+        {y: '2012 Q3', item1: 4820},
+        {y: '2012 Q4', item1: 15073},
+        {y: '2013 Q1', item1: 10687},
+        {y: '2013 Q2', item1: 8432}
+      ],
+      xkey: 'y',
+      ykeys: ['item1'],
+      labels: ['Item 1'],
+      lineColors: ['#3c8dbc'],
+      hideHover: 'auto'
+    });
+
+	line.redraw();*/
+
+	line.redraw();
+});
--- resources/views/comercial/index.blade.php
+++ resources/views/comercial/index.blade.php
@@ -4,6 +4,47 @@
     <h1>Comercial</h1>
 @stop
 @section('content')
+	<h2 class="page-header">Faturamento/Financeiro</h2>
+	<div class="row">
+		<div class="col-md-8">
+			<div class="box box-solid bg-teal-gradient">
+				<div class="box-header">
+					<i class="fa fa-th"></i>
+					<h3 class="box-title">Faturamento</h3>
+					<div class="box-tools pull-right">
+						<button type="button" class="btn bg-teal btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
+                		</button>
+                		<button type="button" class="btn bg-teal btn-sm" data-widget="remove"><i class="fa fa-times"></i>
+                		</button>
+					</div>
+				</div>
+				<div class="box-body border-radius-none">
+					<div class="chart" id="faturamento" style="height: 250px;"></div>
+				</div>
+				<div class="box-footer no-border">
+					<h5 class="box-title" style="color:black">Faturamento Hoje</h5>
+					<div class="row">
+						<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
+							<input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"
+                         data-fgColor="#39CCCC">
+                         <div class="knob-label">Proel Hospitalar</div>
+						</div>
+						<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
+							<input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60"
+                         data-fgColor="#39CCCC">
+                         <div class="knob-label">SH</div>
+						</div>
+						<div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
+							<input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60"
+                         data-fgColor="#39CCCC">
+                         <div class="knob-label">Proel Rep.</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<h2 class="page-header">Ações Comerciais</h2>
 	<div class="row">
 		<div class="col-md-6">
 			<div class="box box-info">
@@ -49,11 +90,13 @@
 		}
 	
 	</style>
+	<link rel="stylesheet" href="{{url('plugins/bower_components/morrisjs/morris.css')}}">
 	
 @endpush
 
 @push('js')
 	<script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
+	
 	
 	<script>
 		$(document).ready(function(){
@@ -79,5 +122,6 @@
 			
 		});	
 	</script>	
+	<script src="{{url('plugins/bower_components/jquery-knob/dist/jquery.knob.min.js')}}"></script>
 	@include('sweetalert::alert')
 @endpush
